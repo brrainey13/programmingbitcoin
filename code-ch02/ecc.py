1458577df86dd0ae52b51189973cf700437340c6
Blake Rainey <brrainey13@gmail.com>
2021-07-06 17:15:26 -0400

    another commit

--- code-ch02/ecc.py
+++ code-ch02/ecc.py
@@ -181,12 +181,18 @@ class Point:
         # y3=s*(x1-x3)-y1
 
         # Case 3: self == other
+        if self == other:
+            s = (3*self.x**2 + self.a)/(2*self.y)
+
+            x = s**2 - 2*self.x
+
+            y = s*(self.x - x) - self.y
+            
+            return self.__class__(x,y,self.a,self.b)
         # Formula (x3,y3)=(x1,y1)+(x1,y1)
         # s=(3*x1**2+a)/(2*y1)
         # x3=s**2-2*x1
         # y3=s*(x1-x3)-y1
-
-        raise NotImplementedError
 
 
 class PointTest(TestCase):
